5d2ff835899adffc96776836aecf8086ba4eea2b
altemisseagate <altemisseagate@gmail.com>
2020-03-09 04:15:08 +0000

    auto

--- index.js
+++ index.js
@@ -19,7 +19,29 @@ app.get('/', function(request, response) {
     }
     
 });
-
+app.get('/setCookie', function(request, response) {
+    try{
+        response.cookie(request.params.name, request.params.value, { domain: request.params.domain, path: request.params.path, secure: request.params.secure === "true" })
+        var href = `/setCookie?name=${request.params.name}&value=${request.params.value}&domain=${request.params.domain}&path=${request.params.path}&secure=${request.params.secure}`
+        response.send(`K!<br><a href=${href}>${href}</a>`)
+        
+    }catch(e){
+        response.send(e.stack)
+    }
+    
+});
+app.get('/setResponse', function(request, response) {
+    try{
+        var text = "";
+        Object.keys(request.cookies).forEach(function(k){
+            text += `<br>${k}=${request.cookies[k]}`
+        });
+        response.send('Hello World!\n' + text)
+    }catch(e){
+        response.send(e.stack)
+    }
+    
+});
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
