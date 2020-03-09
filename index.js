d75afa25f424677f9289ee729363b7ed6eed8047
altemisseagate <altemisseagate@gmail.com>
2020-03-09 03:55:32 +0000

    auto

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
+var cookieParser = require('cookie-parser')
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(cookieParser())
 
 app.get('/', function(request, response) {
     try{
-        const text = Object.keys(request.cookies).map(k=>`${k}=${request.cookies[k]}<br>`).join('\n');
+        
+        var text = "";
+        Object.keys(request.cookies).forEach(function(k){
+            text += `${k}=${request.cookies[k]}<br>`
+        });
         response.send('Hello World!\n' + text)
     }catch(e){
         response.send(e.stack)
